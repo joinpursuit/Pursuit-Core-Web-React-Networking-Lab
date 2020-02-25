e2a023ceaac0f5f62933fed033ad31d662144602
ashya <ashyamanning@pursuit.org>
2020-02-24 21:24:00 -0500

    create files

---
+++ blackjack/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+  state = {}
+  
+  render() {
+    return (
+      <div className="App">
+  
+      </div>
+    );
+  }
+}
+
+export default App;
