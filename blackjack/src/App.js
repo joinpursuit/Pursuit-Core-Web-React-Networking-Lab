b93c5a35bf5bfe3a88e74661f01325cfb3ba5f49
ashya <ashyamanning@pursuit.org>
2020-02-24 21:59:52 -0500

    grab new deck api

--- blackjack/src/App.js
+++ blackjack/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-
+import Menu from './components/Menu';
+import axios from 'axios';
 
 
 class App extends React.Component {
-  state = {}
+  state = {deckId: "", currentCards: [], newGame: false}
+  
+  handleNewDeck() {
+    this.getNewDeck();
+  }
+  getNewDeck = async () => {
+    try {
+      let res = await axios.get("https://deckofcardsapi.com/api/deck/new/");
+      let deck_id = res.data.deck_id;
+      this.setState({deckId: deck_id})
+    } catch (err) {
+      this.setState({deckId: ""});
+      console.log(err);
+    }
+  }
   
   render() {
     return (
       <div className="App">
-  
+        <Menu />
+        <button onClick={this.handleNewDeck}>Generate Deck</button>
       </div>
     );
   }
---
+++ blackjack/src/components/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import axios from 'axios';
+
+class Menu extends React.Component {
+    state = { deckId: ""}
+    
+
+    render() {
+       return(
+           <div>
+            
+           </div>
+       ) 
+    }
+}
+
+export default Menu;
