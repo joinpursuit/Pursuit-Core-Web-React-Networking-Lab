c4fa949eff9c59d7e39a530ad5f8488ddee050be
maria liza <mariamartinez@pursuit.org>
2020-02-25 21:58:59 -0500

    almost finished with lab, need one more function

--- blackjack-api/src/menu.js
+++ blackjack-api/src/menu.js
@@ -1,23 +1,46 @@
 import React from "react";
-import Cards from "./Cards";
+// import Cards from "./Cards";
+import axios from "axios";
+import Card from "./cardImage";
 
 class Menu extends React.Component {
-  state = { deckId: "" };
+  state = { deckId: "", cards: [] };
 
   handleChange = e => {
     e.preventDefault();
-    const { deckId } = this.state;
+    // const { deckId } = this.state;
     // this.setState({ [e.target.parentNode.children[1].value]: e.target.value });
     this.setState({ deckId: e.target.value });
-    debugger;
+    // debugger;
+  };
+
+  userCards = async id => {
+    id = this.state.deckId;
+    console.log(id);
+    try {
+      let res = await axios.get(
+        `https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`
+      );
+      //   debugger;
+      let cards = res.data.cards;
+      this.setState({ cards: cards });
+    } catch (error) {
+      console.log(error);
+      this.setState({ cards: [] });
+    }
   };
 
   render() {
+    let userCardPics = this.state.cards.map((el, index) => {
+      return <Card img={el.image} key={index} />;
+    });
+
     return (
       <div>
+        {userCardPics}
         <label>Enter Deck ID: </label>
         <input value={this.state.deckId} onChange={this.handleChange}></input>
-        <button onClick={this.handleChange}>Draw a Card</button>
+        <button onClick={this.userCards}>Draw a Card</button>
       </div>
     );
   }
