8e0c1c6bd17e330361bf3c52374affcd5e664bca
Henrynunez112 <henrynunez@pursuit.org>
2020-02-25 20:05:13 -0500

    debugging

--- blackjack_app/src/Component/BlackJack.js
+++ blackjack_app/src/Component/BlackJack.js
@@ -15,6 +15,18 @@ export class BlackJack extends Component {
         this.setState({
             deckId: res.data.deck_id
         })
+    
+    }
+    drawCards = async (e) =>{
+        e.preventDefault()
+
+        let res = await axios.get(`https://deckofcardsapi.com/api/deck/${e.target.elements[0].value}/draw/?count=2`)
+        return res.data.cards.map((card)=>{
+            return <Cards />
+        })
+            // debugger
+            // key={card.image} image={card.image}
+
     }
 
     render() {
@@ -24,8 +36,11 @@ export class BlackJack extends Component {
             <div>
                 <button onClick={this.generateDeckId}>Generate Deck ID</button>
                 <p>{deckId}</p>
-                <button onClick={Cards.this.getCards}>Draw Card</button>
-                {/* <Cards deckId={deckId} /> */}
+                {/* <button onClick={Cards.this.getCards}>Draw Card</button> */}
+                <form onSubmit={this.drawCards}>
+                <input type="text" placeholder="Enter Deck Id" defaultValue=""></input>
+                <button>Draw</button>
+                </form>
             </div>
         )
     }
--- blackjack_app/src/Component/Cards.js
+++ blackjack_app/src/Component/Cards.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
 
 export class Cards extends Component {
-    state = {
-        cardImg: [],
-        count: 2
-    }
 
-    
-    getCards = async (deckId, count) =>{
-        let carUrl = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${count}`
-
-        debugger
-    }
     render() {
+        console.log(this.props)
         debugger
         return (
             <div>
-                {this.getCards(this.props.deckId, this.state.count)}
+                Hello World
             </div>
         )
     }
 }
 
 export default Cards
+
