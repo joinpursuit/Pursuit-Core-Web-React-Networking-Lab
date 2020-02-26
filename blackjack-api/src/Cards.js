206643b4075339d8a46436bd732107a800e5738f
maria liza <mariamartinez@pursuit.org>
2020-02-25 19:24:18 -0500

    get image to appear for card drawn

--- blackjack-api/src/Cards.js
+++ blackjack-api/src/Cards.js
@@ -1,29 +1,35 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import Card from "./cardImage";
 
 class Cards extends React.Component {
-    state = {img: ""}
+  state = { img: "" };
 
-    getCardsPics = async () => {
-        try{
-            let res = await axios.get(`https://deckofcardsapi.com/api/deck/new/draw/?count=1`)
-            debugger
-        } catch(error) {
-            console.log(error)
-        }
+  //maybe comp did mount and getimage fn
+
+  getCardsPics = async () => {
+    try {
+      let res = await axios.get(
+        `https://deckofcardsapi.com/api/deck/new/draw/?count=1`
+      );
+      // debugger
+      this.setState({ img: res.data.cards[0].images.png });
+      //   console.log(img);
+    } catch (error) {
+      console.log(error);
+      this.setState({ img: "" });
     }
+  };
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.getCardsPics}>Draw A Card</button>
-            </div>
-        )
-    }
-
-
+  render() {
+    return (
+      <div>
+        <Card img={this.state.img} />
+        <button onClick={this.getCardsPics}>Draw A Card</button>
+        {/* <img  */}
+      </div>
+    );
+  }
 }
-
-
 
 export default Cards;
