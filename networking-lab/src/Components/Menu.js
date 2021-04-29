33f405f8eda6b7d87802d5379a681d4b20585afe
Alexander <alexanderl96@yahoo.com>
2021-04-29 16:38:24 -0400

    jhjs

---
+++ networking-lab/src/Components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Menu() {
+    return (
+        <div>
+            <button>Generate Deck</button>
+            <label>Input Existing Deck</label>
+            <input></input>
+            <button>Draw</button>
+        </div>
+    )
+}
