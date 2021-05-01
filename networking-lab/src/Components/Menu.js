5fabc230d73bbd852eb2f157aa5513e0ea7162d1
Alexander <alexanderl96@yahoo.com>
2021-05-01 16:47:43 -0400

    hghf

--- networking-lab/src/Components/Menu.js
+++ networking-lab/src/Components/Menu.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import "./menu.css"
 
-export default function Menu() {
+export default function Menu(props) {
+    
     return (
-        <div>
-            <button>Generate Deck</button>
+        <div className="menu">
+            <button onClick={props.newDeck}>Generate Deck</button>
             <label>Input Existing Deck</label>
-            <input></input>
-            <button>Draw</button>
+            <input type="text" value={props.deckid} onChange={props.handleBTn}></input>
+            <button onClick={props.drawCard}>Draw</button>
         </div>
     )
 }
