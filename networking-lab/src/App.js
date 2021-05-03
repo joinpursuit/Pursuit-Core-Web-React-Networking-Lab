b9ae2889b61702bc19fd3ee05357431f2b0dd076
Jon Narine <jonnicwolf@gmail.com>
2021-05-02 22:13:46 -0400

    commit before submit

--- networking-lab/src/App.js
+++ networking-lab/src/App.js
@@ -1,33 +1,58 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import Menu from "./components/Menu";
+import Hand from "./components/Hand";
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      deck: [],
+      deckId: "",
+      cards: [],
     };
   }
 
   touchCardsApi = async () => {
     try {
-      const deck = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-      console.log(deck.data)
-      let draw_Seto_Kaiba = await axios.get(`https://deckofcardsapi.com/api/deck/${deck.data.deck_id}/draw/?count=1`)
-      console.log(draw_Seto_Kaiba.data)
-      
-      
-    } catch (err) {console.log(err)}
-  }
-  
+      const deck = await axios.get(
+        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+      );
+      return deck;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  draw_Seto_Kaiba = async ({ touchCardsApi }) => {
+    try {
+      const draw = await axios.get(
+        `https://deckofcardsapi.com/api/deck/${touchCardsApi.data.deck_id}/draw/?count=1`
+      );
+
+      const card = draw.data.cards[0].image;
+      console.log("card", card);
+
+      return this.setState({
+        deckId: touchCardsApi.data.deck_id,
+        cards: card,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   render() {
-    
+    const { deckId, cards } = this.state;
+
     return (
-      <div>
-        yo
-        <button onClick={this.touchCardsApi}>click</button>
-      </div>
+      <>
+        <Menu
+          deckId={deckId}
+          cards={cards}
+          touchCardsApi={this.touchCardsApi}
+        />
+        <Hand cards={cards} />
+      </>
     );
   }
-  
 }
--- networking-lab/src/components/Hand.js
+++ networking-lab/src/components/Hand.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Hand(props) {
-    console.log(props.card)
+    
     return <img
         src={props.cards}
         alt='Playing Card' />
